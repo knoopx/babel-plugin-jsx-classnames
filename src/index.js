84542bdaacd74fe4924714a61f22b5796080cb68
Victor Martinez <knoopx@gmail.com>
2021-05-06 16:14:35 +0200

    chore: update dependencies

--- src/index.js
+++ src/index.js
@@ -1,20 +1,20 @@
-import jsx from '@babel/plugin-syntax-jsx'
-import { addDefault } from '@babel/helper-module-imports'
+import jsx from "@babel/plugin-syntax-jsx"
+import { addDefault } from "@babel/helper-module-imports"
 
-export default function({ types: t }) {
+export default function ({ types: t }) {
   return {
     inherits: jsx,
     visitor: {
       JSXAttribute(path) {
         if (
-          t.isJSXIdentifier(path.node.name, { name: 'className' }) &&
+          t.isJSXIdentifier(path.node.name, { name: "className" }) &&
           t.isJSXExpressionContainer(path.node.value) &&
           !t.isStringLiteral(path.node.value.expression)
         ) {
           path.node.value = t.JSXExpressionContainer(
             t.callExpression(
-              addDefault(path, 'classnames', {
-                nameHint: 'classNames',
+              addDefault(path, "classnames", {
+                nameHint: "classNames",
               }),
               [path.node.value.expression],
             ),
--- test/test.js
+++ test/test.js
@@ -1,30 +1,31 @@
-import { transform } from '@babel/core'
-import jsxClassNames from '../src'
+import { transform } from "@babel/core"
+
+import jsxClassNames from "../src"
 
 function apply(code) {
   return transform(code, { plugins: [jsxClassNames] }).code
 }
 
-describe('babel-plugin-jsx-classnames', () => {
-  it('ignores non-className props', () => {
+describe("babel-plugin-jsx-classnames", () => {
+  it("ignores non-className props", () => {
     expect(apply(`<div anotherProp="a" />`)).toEqual(`<div anotherProp="a" />;`)
   })
 
-  it('ignores string-literal className prop', () => {
+  it("ignores string-literal className prop", () => {
     expect(apply(`<div className="a" />`)).toEqual(`<div className="a" />;`)
   })
 
-  it('ignores string className prop within classNames', () => {
+  it("ignores string className prop within classNames", () => {
     expect(apply(`<div className={"a"} />`)).toEqual(`<div className={"a"} />;`)
   })
 
-  it('wraps array className prop within classNames', () => {
+  it("wraps array className prop within classNames", () => {
     expect(apply(`<div className={["a"]} />`)).toEqual(
       `import _classNames from "classnames";\n<div className={_classNames(["a"])} />;`,
     )
   })
 
-  it('wraps object className prop within classNames', () => {
+  it("wraps object className prop within classNames", () => {
     expect(apply(`<div className={{a: true}} />`)).toEqual(
       `import _classNames from "classnames";\n<div className={_classNames({\n  a: true\n})} />;`,
     )
